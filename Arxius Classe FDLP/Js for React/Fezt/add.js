e314c85c7c6b70786f1e78ff2e95b1a7e5b3d9b7
llopFilms <smdalbars@gmail.com>
2022-08-25 21:15:10 +0200

    Afegir carpeta 'Js per React': - Pràctica Fzet - Pràctica Azaustre

---
+++ Arxius Classe FDLP/Js for React/Fezt/add.js
@@ -0,0 +1,11 @@
+export const suma = (x = 0, y = 0) => x + y;
+
+export const multi = (x, y) => x * y;
+
+const titolI = "Hola a tothom";
+const actiuI = true;
+const punts = [10, 20, 30];
+
+export { titolI, actiuI, punts };
+
+export default suma;

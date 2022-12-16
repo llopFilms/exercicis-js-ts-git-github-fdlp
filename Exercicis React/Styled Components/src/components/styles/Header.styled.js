2e9e63441713dfa9c5f03b1a0642e9f58197e9aa
llopFilms <smdalbars@gmail.com>
2022-12-16 23:47:33 +0100

    ok

--- Exercicis React/Styled Components/src/components/styles/Header.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledHeader = styled.header`
-  /* background-color:${({ bg }) => bg}; */
-  background-color: ${({theme}) => theme.colors.header};
-  padding: 40px 0;
-
-`;

06c4226ad881ce37f236560d8bfc70a102cc315d
llopFilms <smdalbars@gmail.com>
2022-12-29 01:54:36 +0100

    intersectionObersver

---
+++ intersectionObserver/js/app.js
@@ -0,0 +1,16 @@
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+
+const carregarImatge = (entrades, observador) => {
+  entrades.forEach((entrada) => {
+    if (entrada.isIntersecting) entrada.target.classList.add("visible");
+    else entrada.target.classList.remove("visible"); });
+};
+
+const observador = new IntersectionObserver(carregarImatge, {
+  rootMargin: "500px 100px 0px 0px",
+  threshold: 1,
+});
+
+observador.observe(img1);
+observador.observe(img2);

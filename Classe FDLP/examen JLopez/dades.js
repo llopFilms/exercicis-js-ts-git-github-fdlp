0126ddf055fc66d87667316c57057066add9381c
llopFilms <llopfilms@gmail.com>
2023-03-14 12:49:30 +0100

    Ordre a Exercicis Js

--- Classe FDLP/examen JLopez/dades.js
+++ Classe FDLP/examen JLopez/dades.js
@@ -1,40 +1,38 @@
 const treballadors = [
-   {
-      nom : "Ana",
-      cognom: "Puig",
-      carrec: "venedor(a)",
-      disponibilitatTrasllat : true
-   },
-   {
-      nom : "Lluis",
-      cognom: "Soto",
-      carrec: "venedor(a)",
-      disponibilitatTrasllat : false
-   },
-   {
-      nom : "Cristina",
-      cognom: "Tomas",
-      carrec: "administratiu(a)",
-      disponibilitatTrasllat : false
-   },
-   {
-      nom : "Andreu",
-      cognom: "Ricart",
-      carrec: "venedor(a)",
-      disponibilitatTrasllat : true
-   },
-   {
-      nom : "Pep",
-      cognom: "Vila",
-      carrec: "venedor(a)",
-      disponibilitatTrasllat : false
-   },
-   {
-      nom : "Núria",
-      cognom: "Roure",
-      carrec: "administratiu(a)",
-      disponibilitatTrasllat : true
-   },
-
-]
-
+	{
+		nom: "Ana",
+		cognom: "Puig",
+		carrec: "venedor(a)",
+		disponibilitatTrasllat: true,
+	},
+	{
+		nom: "Lluis",
+		cognom: "Soto",
+		carrec: "venedor(a)",
+		disponibilitatTrasllat: false,
+	},
+	{
+		nom: "Cristina",
+		cognom: "Tomas",
+		carrec: "administratiu(a)",
+		disponibilitatTrasllat: false,
+	},
+	{
+		nom: "Andreu",
+		cognom: "Ricart",
+		carrec: "venedor(a)",
+		disponibilitatTrasllat: true,
+	},
+	{
+		nom: "Pep",
+		cognom: "Vila",
+		carrec: "venedor(a)",
+		disponibilitatTrasllat: false,
+	},
+	{
+		nom: "Núria",
+		cognom: "Roure",
+		carrec: "administratiu(a)",
+		disponibilitatTrasllat: true,
+	},
+];
